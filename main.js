8ab6a1aa436c6c90412cd129d18137de0455135d
Brandon Simmons <brandon@binarybeast.com>
2016-12-08 20:01:32 -0800

    main.js tweaks

--- main.js
+++ main.js
@@ -4,12 +4,23 @@ const roles = {
     builder: require('role.builder'),
 };
 
+const spawn = Game.spawns.s1;
+
 function printRoomEnergy() {
     console.log(`Room [${name}] has ${Game.rooms[name].energyAvailable} energy`);
 }
 
 function _harvesters() {
     return _.filter(Game.creeps, (creep) => creep.memory.role === 'harvester');
+}
+
+
+function _builders() {
+    return _.filter(Game.creeps, (creep) => creep.memory.role === 'builder');
+}
+
+function _upgraders() {
+    return _.filter(Game.creeps, (creep) => creep.memory.role === 'builder');
 }
 
 /**
@@ -35,13 +46,30 @@ function _cleanup() {
 module.exports.loop = function() {
     _cleanup();
 
-    let harvesters = _harvesters();
+    let harvesters = _harvesters(),
+        builders = _builders(),
+        upgraders = _upgraders();
+
     console.log(`Harvester count: ${harvesters.length}`);
 
-    // Need at least 2 harvesters
+    // Need at least 2 harvesters, builders, and upgrades
     if (harvesters.length < 2) {
-        let newName = Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], undefined, { role: 'harvester' });
-        console.log(`Spawning new harvester (${newName})`);
+        let newName = spawn.createCreep([WORK, WORK, CARRY, CARRY, MOVE, MOVE], undefined, { role: 'harvester' });
+        if (!(newName < 0)) {
+            console.log(`Spawning new harvester (${newName})`);
+        }
+    }
+    if (builders.length < 2) {
+        let newName = spawn.createCreep([WORK, WORK, CARRY, CARRY, MOVE, MOVE], undefined, { role: 'builder' });
+        if (!(newName < 0)) {
+            console.log(`Spawning new harvester (${newName})`);
+        }
+    }
+    if (builders.length < 2) {
+        let newName = spawn.createCreep([WORK, WORK, CARRY, CARRY, MOVE, MOVE], undefined, { role: 'upgrader' });
+        if (!(newName < 0)) {
+            console.log(`Spawning new harvester (${newName})`);
+        }
     }
 
     _.each(Game.creeps, function(creep) {
@@ -50,9 +78,29 @@ module.exports.loop = function() {
         }
     });
 
-}
+
+    /*
+    let tower = Game.getObjectById('f9c4e0babc76c9ed0244b94a');
+    if(tower) {
+        let foe     = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS),
+            friend  = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+                filter: (structure) => structure.hits < structure.hitsMax
+            });
+
+        if( friend ) {
+            tower.repair(friend);
+        }
+
+        if( foe ) {
+            tower.attack(foe);
+        }
+    }*/
+};
 
 
 
 // Create turret
 // Game.spawns.Spawn1.room.createConstructionSite( 23, 22, STRUCTURE_TOWER );
+
+// Create upgrader
+// Game.spawns.Spawn1.createCreep([WORK, WORK, MOVE], undefined, {role: 'upgrader'})
